edb79ebf86cf8461c4bd6bf9bf08a9f93a140747
Pablo <ppignolo@huffman.csic.edu.uy>
2020-02-10 18:39:21 -0300

    Se añaden dos archivos de configuración para setear variables del sistema y variables de la conexión con la ANII'

---
+++ configAniiVariables.php
@@ -0,0 +1,9 @@
+<?php
+
+  $paginaInicialListaCVsANII = 1;
+  $paginaFinalListaCVsANII = 238;
+  $usuarioANII = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'; //SUSTITUIR ESTA VARIABLE POR EL USUARIO OTORGADO POR LA ANII PARA ACCEDER AL Webservice del CVUy
+  $passwordANII = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'; //SUSTITUIR ESTA VARIABLE POR LA PASSWORD OTORGADA POR LA ANII PARA ACCEDER AL Webservice del CVUy
+
+
+ ?>
---
+++ configSystemVariables.php
@@ -0,0 +1,14 @@
+<?php
+
+ini_set('max_execution_time', 0);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+ini_set('memory_limit', '4024M'); // or you could use 1G
+error_reporting(E_ALL);
+
+$dbHOST = '127.0.0.1';
+$dbNAME = 'xxxxx';
+$dbUSER = 'xxxxx';
+$dbPASS = 'xxxxx';
+
+ ?>
